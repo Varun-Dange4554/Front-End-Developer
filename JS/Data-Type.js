aedecdd34fb6b6bb1ec970ba6e121a4ac635974d
Varun-Dange4554 <varundange7@gmail.com>
2025-08-25 11:56:16 +0530

    code of the day

--- JS/Data-Type.js
+++ JS/Data-Type.js
@@ -7,9 +7,9 @@ var num = 20;
 
 var bool = true;
 
-var arr = [1,2,3,4];  //array  of number
+var arr = [1,2,3,4,"varun","afan"];  //array  of number
 var arrOfObject = [{ str: 'name' }, { number: 2 }]; // array of object with mix data type
-var obj = { name: 'mehfooz', age: 26 };
+var obj = { name: 'mehfooz', age: 26, address: 'Andheri' };
 
 var und = undefined;
 
--- JS/operators.js
+++ JS/operators.js
@@ -1,23 +1,25 @@
 //operators 
 
+// const { Children } = require("react");
+
 
 //Arithmetic operators
 
-// let a = 5;
+//  let a = 5;
 // let b = 3;
 
 // console.log("a = ",a, "b = ",b);   
 
 // console.log("a + b = ",a + b );       //Addition
 // console.log("a - b = ",a - b );       //Subtraction
-// console.log("a * b = ",a * b );       //Multiplication
+//  console.log("a * b = ",a * b );       //Multiplication
 // console.log("a / b = ",a / b );       //Division
 // console.log("a % b = ", a % b);       //modulus % 
 // console.log( "a ** b = ", a ** b);    //Exponentiation
 
 
 
-//Unary Operator
+// Unary Operator
 // let a = 4;
 // let b = 3;
 
@@ -25,8 +27,8 @@
 
 // console.log("--a = ", --a);      //Pre Increment
 
-// console.log("a-- = " ,a--);       //post  Increment
-// console.log("a = " ,a);
+// console.log("b-- = " ,b--);       //post  Increment
+
 
 
 
@@ -42,14 +44,14 @@
 
 //Comparison Operators
 
-// (Equal to ==),   (Equal to & type ===),  (Not equal to !=),    (Not equal to & type !==)
+// (Equal to ==),   (data & type ===),  (Not equal to !=),    (Not equal to & type !==)
 
 
 // let a = 3;
 // let b = 4;
 
-// console.log("4 == 3",   a == b);
-// console.log("4 === 4",   a === b);
+// console.log("a == b",   a == b);
+// console.log("a === b",   a === b);
 // console.log(typeof a);
 // console.log("a != b", a != b);
 // console.log("a !== b " ,a !== b)
@@ -61,15 +63,15 @@
 
 // Logical Operators   (&& AND),  (|| OR ), (! NOT)
 
-// let a = 9;
+// let a = 4;
 // let b = 4;
 // (&& And)
-
-// condition1 = a === b;
-// condition2 = a !== b;
-// console.log(condition1 && condition2);
+// condition1 = a==b;
+// condition2 = a===b;
+// condition3 = a==b && a===b; 
 // console.log(condition1);
 // console.log(condition2);
+// console.log(condition3);
 
 // (|| OR )
 // console.log(a > b || a < b);  
@@ -81,10 +83,54 @@
 //(! NOT )
 // console.log("(! a == 9)" ,(! a > b) ); //true
 
+//Bitwise operators
+
+//conversion into number to binary and binary to number....
+// let number = 50;
+// let binaryConv = number.toString(2);
+// console.log('binaryConv',binaryConv);
 
 
+//binary to number
 
-   for(let i = 1; i<= 10;i++){
-    console.log('squre of',i, 'is',  (i ** 3));
-  } 
+// let bin_To_num = parseInt(binaryconv,2);
+// console.log('bin_to_num:', binaryconv);
+/*
+   & --> AND 
+    | --> OR
+    ~ -->NOT
     
+    */
+  //  let num66 = 15;
+  //  let num77 = 12;
+
+  //  let binaryConv1 = num66.toString(2);
+  //  console.log("binaryCoven",binaryConv1);
+  //  let binaryConv2 = num77.toString(2);
+  //  console.log("binaryConve",binaryConv2);
+  //  let and = ~num77;
+
+  // let and = num66 & num77;
+  // console.log("and",and);
+
+  // let and = num66 | num77;
+  // console.log("and ",and);
+
+
+
+
+  // speard and rest operator
+
+
+//speard operator
+  // let arr = [1,2,3,4];
+  //  let newArr = [...arr,5,6,7,8];
+  //  console.log('newArr',newArr);
+ 
+   //rest operator
+
+   const sum = (...xyz){
+    // console.log("a", a,b,c);
+    console.log('args:',xya);
+   } 
+   sum[1,2,3,4];
