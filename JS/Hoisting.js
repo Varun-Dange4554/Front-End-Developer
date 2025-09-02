6b0d82fd3f8369fe9226b48a7958a66e3d5cb2cf
Varun-Dange4554 <varundange7@gmail.com>
2025-09-02 11:11:53 +0530

    code of the day

--- JS/Hoisting.js
+++ JS/Hoisting.js
@@ -30,3 +30,80 @@ console.log('arr:',arr)
 
 // console.log([]===[]);
 // console.log({}=={});
+
+
+
+
+
+
+
+
+
+
+// immutable [call by value ] [primitive]
+
+let name3=`varun`; //a
+
+let first=name3[0]; //b 
+console.log(first);
+
+first='k'; // b update 
+
+console.log(first);
+
+name3[0]=`k`
+
+console.log(name3[0]);
+console.log(name3);
+
+// console.log(name3[0]);
+// console.log(name3[1]);
+// console.log(name3[2]);
+// console.log(name3[3]);
+// console.log(name3[4]);
+
+
+
+/*
+number
+string 
+boolean
+*/
+
+
+
+
+
+// mutable [call by reference] [non-primitive]
+
+let arr=[1, 2, 3, 4,];
+let newData=arr[0];
+
+arr[0]=5
+
+console.log(newData);
+console.log(arr)
+
+
+//object
+
+let object={
+    name:`varun`
+
+}
+object.name="Dange"
+console.log(object);
+
+
+//for loop 
+
+for (var i=1; i<=2;i++){
+    console.log(i);
+}
+console.log(i);
+
+/*
+OBJECT
+ARRAY
+FUNCTION
+*/
