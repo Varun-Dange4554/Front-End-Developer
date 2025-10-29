ee19248fa15ca371ee33cfb8cff1e9c17aeb5ead
Varun-Dange4554 <varundange7@gmail.com>
2025-10-29 13:29:20 +0530

    code of the day

--- JS/Movie_api_app/Movie_api.js
+++ JS/Movie_api_app/Movie_api.js
@@ -1,18 +1,46 @@
-const API_BASE_URL = "https://www.omdbapi.com/";
+const searchMovie = () => {
+  let query = document.getElementById("searchInput").value;
 
-const dataFetch = () => {
-  fetch(`${API_BASE_URL}?i=tt3896198&apikey=37f6b325`)
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error(`HTTP error! Status: ${res.status}`);
-      }
-      return res.json();
-    })
-    .then((res) => console.log(res))
-    .catch((err) => console.error("Fetch error:", err))
-    .finally(() => {
-      console.log("API fetch completed");
-    });
+  if(query === ""){
+    alert("Please enter movie name");
+    return;
+  }
+
+  fetch(`http://www.omdbapi.com/?s=${query}&apikey=37f6b325`)
+  .then((res) => res.json())
+  .then((data) => {
+    if(data.Response === "False"){
+      document.getElementById("movie").innerHTML = "<h2>No movie found</h2>";
+      return;
+    }
+    Render_UI(data.Search);
+  })
+  .catch((err) => console.log(err));
 };
 
-dataFetch();
+
+const Render_UI = (movies) => {
+    let mainDiv = document.getElementById("movie");
+    mainDiv.innerHTML = "";
+
+    movies.map((Element) => {
+        let cardDiv = document.createElement('div');
+
+        let img = document.createElement('img');
+        let title = document.createElement('h3');
+        let type = document.createElement('p');
+        let year = document.createElement('p');
+        let imdbId = document.createElement('p'); // ✅ correct variable
+
+        img.src = Element.Poster;
+        title.innerText = Element.Title;
+        type.innerText = "Type: " + Element.Type;
+        year.innerText = "Year: " + Element.Year;
+        imdbId.innerText = "IMDB ID: " + Element.imdbID; // ✅ correct key
+
+        cardDiv.className = "card-div";
+
+        cardDiv.append(img, title, type, year, imdbId);
+        mainDiv.append(cardDiv);
+    });
+};
