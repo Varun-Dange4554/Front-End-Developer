c5cd909afea580b92fe8174dada07cec86d09196
Varun-Dange4554 <varundange7@gmail.com>
2025-12-15 23:43:20 +0530

    code

---
+++ React_js/todo-test/src/App.jsx
@@ -0,0 +1,14 @@
+import  TodoInput  from './Components/TodoInput'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <TodoInput />
+    </> 
+  )
+}
+
+export default App
---
+++ React_js/todo-test/src/Components/TodoInput.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TodoInput = () => {
+  return (
+    <div>
+      <h1>Todo Input</h1>
+      <input type="text"  placeholder='enter your todo'/>
+                                        
+    </div>
+  )
+}
+
+export default TodoInput
